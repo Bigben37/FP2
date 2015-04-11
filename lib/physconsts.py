a36e315ad78ad78bc405fe31809c0ea4feadcea2
Bigben37 <benjamin@dierottlers.de>
2015-04-11 21:47:55 +0200

    added file for physical constants

---
+++ lib/physconsts.py
@@ -0,0 +1,15 @@
+"""physical constants"""
+
+__author__ = "Benjamin Rottler (benjamin@dierottlers.de)"
+
+# planck constant
+h_Js = 6.62606957e-34  # in J*s
+h_eVs = 4.135667516e-15  # in eV*s
+
+# reduced planck constant
+hbar_Js = 1054571726e-34  # in J*s
+hbar_eVs = 6.58211928e-16  # in eV*s
+
+# bohr magneton
+mub_JT = 9.27400968e-24  # in J/T
+mub_eVT = 5.7883818066e-5  # in eV/T
