bfa98359a8fc4c67748d06f5ccd19730731801d5
Bigben37 <benjamin@dierottlers.de>
2015-04-14 10:31:18 +0200

    added missing dot

--- lib/physconsts.py
+++ lib/physconsts.py
@@ -7,7 +7,7 @@ h_Js = 6.62606957e-34  # in J*s
 h_eVs = 4.135667516e-15  # in eV*s
 
 # reduced planck constant
-hbar_Js = 1054571726e-34  # in J*s
+hbar_Js = 1.054571726e-34  # in J*s
 hbar_eVs = 6.58211928e-16  # in eV*s
 
 # bohr magneton
