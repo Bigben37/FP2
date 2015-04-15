5baee5849264d0e418f93a7c458739d7cb2064cd
Bigben37 <benjamin@dierottlers.de>
2015-04-15 11:14:18 +0200

    cleaned up part 3 memory access error still unsolved

--- 0407-Optisches Pumpen/src/part3.py
+++ 0407-Optisches Pumpen/src/part3.py
@@ -3,11 +3,74 @@ __author__ = "Benjamin Rottler (benjamin@dierottlers.de)"
 
 import os
 from numpy import sqrt
-from functions import loadCSVToList, avgerrors, setupROOT
+from functions import loadCSVToList, setupROOT
 from txtfile import TxtFile
 from physconsts import h_Js, mub_JT
 from op import OPData, inductorIToB, prepareGraph
-from ROOT import TCanvas, TPad, TGraphErrors, TGaxis, gStyle, gROOT
+from ROOT import TCanvas, TPad, TGaxis, gStyle
+
+
+def makeTwoScalesGraph(file):
+    print(file)
+    ch1 = OPData.fromPath('../data/part3/%s' % file, 1)
+    ch2 = OPData.fromPath('../data/part3/%s' % file, 2)
+
+    print('make canvas + pad')
+    c = TCanvas('c-%s' % file, '', 1280, 720)
+    pad = TPad('pad-%s' % file, '', 0, 0, 1, 1)
+    pad.Draw()
+    pad.cd()
+
+    print('frame')
+    frame = pad.DrawFrame(0, -0.25, 0.051, 0.25)
+    frame.SetXTitle('t / s')
+    frame.GetXaxis().CenterTitle()
+    frame.SetYTitle('Spannung Spule 2: U_{2} / V')
+    frame.GetYaxis().CenterTitle()
+
+    print('g1')
+    g1 = ch1.makeGraph('g1-%s' % file)
+    prepareGraph(g1)
+    g1.Draw('PX')
+
+    print('overlay')
+    c.cd()
+    overlay = TPad('overlay-%s' % file, '', 0, 0, 1, 1)
+    overlay.SetFillStyle(4000)  # transparent
+    overlay.SetFillColor(0)  # white
+    overlay.SetFrameFillStyle(4000)  # transparent
+    overlay.Draw()
+    overlay.cd()
+
+    print('g2')
+    g2 = ch2.makeGraph('g2-%s' % file)
+    prepareGraph(g2)
+    g2.SetMarkerColor(2)
+    g2ymin = min(ch2.getY())
+    xmin = pad.GetUxmin()
+    xmax = pad.GetUxmax()
+    ymin = 1.1 * g2ymin
+    ymax = abs(ymin)
+    oframe = overlay.DrawFrame(xmin, ymin, xmax, ymax)
+    oframe.GetXaxis().SetLabelOffset(99)
+    oframe.GetYaxis().SetLabelOffset(99)
+    oframe.GetYaxis().SetTickLength(0)
+    g2.Draw('PX')
+
+    print('axis')
+    axis = TGaxis(xmax, ymin, xmax, ymax, ymin, ymax, 510, "+L")
+    axis.SetLineColor(1)
+    axis.SetLabelColor(2)
+    axis.SetTitle('Spannung Photodiode: U_{P} / V')
+    axis.SetTitleColor(2)
+    axis.CenterTitle()
+    axis.SetTitleOffset(1.2)
+    axis.Draw()
+
+    print('print')
+    c.Update()
+    c.Print('../img/part3/%s.pdf' % file[:-4], 'pdf')
+
 
 def makeGraphs():
     gStyle.SetPadTickY(0)
@@ -15,61 +78,9 @@ def makeGraphs():
     files.sort()
     for file in files:
         if file.endswith('.tab'):
-            ch1 = OPData.fromPath('../data/part3/' + file, 1)
-            ch2 = OPData.fromPath('../data/part3/' + file, 2)
-
-            c = TCanvas('c' + file, '', 1280, 720)
-            pad = TPad('pad' + file, '', 0, 0, 1, 1)
-            pad.Draw()
-            pad.cd()
-
-            frame = pad.DrawFrame(0, -0.25, 0.051, 0.25)
-            frame.SetXTitle('t / s')
-            frame.GetXaxis().CenterTitle()
-            frame.SetYTitle('U_B / V')
-            frame.GetYaxis().CenterTitle()
-
-            g1 = ch1.makeGraph('g1' + file)
-            prepareGraph(g1)
-            g1.Draw('P')
-
-            c.cd()
-            overlay = TPad('overlay' + file, '', 0, 0, 1, 1)
-            overlay.SetFillStyle(4000)  # transparent
-            overlay.SetFillColor(0)  # white
-            overlay.SetFrameFillStyle(4000)  # transparent
-            overlay.Draw()
-            overlay.cd()
-
-            g2 = ch2.makeGraph('g2' + file)
-            prepareGraph(g2)
-            g2.SetMarkerColor(2)
-
-            g2ymin = min(ch2.getY())
-            xmin = pad.GetUxmin()
-            xmax = pad.GetUxmax()
-            ymin = 1.1 * g2ymin
-            ymax = abs(ymin)
-            oframe = overlay.DrawFrame(xmin, ymin, xmax, ymax)
-            oframe.GetXaxis().SetLabelOffset(99)
-            oframe.GetYaxis().SetLabelOffset(99)
-            oframe.GetYaxis().SetTickLength(0)
-
-            g2.Draw('P')
-
-            axis = TGaxis(xmax, ymin, xmax, ymax, ymin, ymax, 510, "+L")
-            axis.SetLineColor(1)
-            axis.SetLabelColor(2)
-            axis.SetTitle('Spannung Photodiode U_{P} / V')
-            axis.SetTitleColor(2)
-            axis.CenterTitle()
-            axis.SetTitleOffset(1.2)
-            axis.Draw()
-
-            c.Update()
-            c.Print('../img/part3/' + file[:-4] + '.pdf', 'pdf')
-    
+            makeTwoScalesGraph(file)
     gStyle.SetPadTickY(1)
+
 
 def evalEtalonData():
     results = []
@@ -106,8 +117,9 @@ def evalEtalonData():
     with TxtFile.fromRelPath('../calc/part3.txt', 'w') as f:
         for result in results:
             for description, values in result:
-                f.writeline('\t', *([description] + list(map(lambda x: '%.2f' % x , values))))
+                f.writeline('\t', *([description] + list(map(lambda x: '%.2f' % x, values))))
             f.writeline('')
+
 
 def main():
     makeGraphs()
