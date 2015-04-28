f0b12a9d31ce4f9e18e87376f5461c0309a7f6ca
Bigben37 <benjamin@dierottlers.de>
2015-04-28 11:54:39 +0200

    added single functions

--- 0427-Z0/src/s_t_fit.py
+++ 0427-Z0/src/s_t_fit.py
@@ -1,7 +1,7 @@
 from z0 import Z0Data
 from functions import setupROOT
 from fitter import Fitter
-from ROOT import gStyle, TCanvas, TLegend # @UnresolvedImport
+from ROOT import gStyle, TCanvas, TLegend, TF1 # @UnresolvedImport
 
 def main():
     data = Z0Data.fromROOTFile('../data/mc/ee.root', 'h3')
@@ -11,16 +11,33 @@ def main():
     hist = datacut.makeHistogramm('hist', 'cos_thet', 200, -1, 1)  # TODO params for hist title (defualt = '')
     hist.Draw()
     
-    fit = Fitter('f', '[0] * (1 + x^{2}) + [1] * (1 - x)^(-2)')
+    xmin, xmax = -0.9, 0.9
+    
+    fit = Fitter('f', '[0] * (1 + x^2) + [1] * (1 - x)^(-2)')
     fit.setParam(0, 's', 100)
     fit.setParam(1, 't', 10)
-    fit.fit(hist, -0.9, 0.9)
+    fit.fit(hist, xmin, xmax)
     fit.saveData('../fit/s_t_fit.txt')
     
-    l = TLegend(0.15, 0.6, 0.5, 0.85)
+    sfunc = TF1('sfunc', '[0]*(1+x^2)', xmin, xmax)
+    sfunc.SetParameter(0, fit.params[0]['value'])
+    sfunc.SetLineWidth(1)
+    sfunc.SetLineColor(3)
+    sfunc.Draw('same')
+    
+    tfunc = TF1('tfunc', '[0]*(1-x)^(-2)', xmin, xmax)
+    tfunc.SetParameter(0, fit.params[1]['value'])
+    tfunc.SetLineWidth(1)
+    tfunc.SetLineColor(4)
+    tfunc.Draw('same')
+    
+    l = TLegend(0.15, 0.6, 0.45, 0.85)
+    l.SetTextSize(0.03)
     l.AddEntry(hist, "ee (mit cut)", 'l')
-    l.AddEntry(fit.function, 'Fit mit N = s (1 + x^2) + t (1 - x)^{-2}', 'l')
+    l.AddEntry(fit.function, 'Fit mit N = s (1 + x^{2}) + t (1 - x)^{-2}', 'l')
     fit.addParamsToLegend(l, [('%.2f', '%.2f'), ('%.2f', '%.2f')], chisquareformat='%.2f')
+    l.AddEntry(sfunc, 's (1 + x^{2})', 'l')
+    l.AddEntry(tfunc, 't (1 - x)^{-2}', 'l')
     l.Draw()
     
     #g.Draw('P')
