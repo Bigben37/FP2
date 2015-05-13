4218ae3d5989c8fbe8c3a79755b127bd5e0fb5a5
Bigben37 <benjamin@dierottlers.de>
2015-05-13 23:19:39 +0200

    fig 5.3 legend + y title

--- 0407-Optisches Pumpen/src/part2.py
+++ 0407-Optisches Pumpen/src/part2.py
@@ -188,68 +188,6 @@ def evalEtalonData():
     return freqCalibration
 
 
-def makeHFSGraph(name, xmin, xmax):
-    ch1 = OPData.fromPath('../data/part2/%s.tab' % name, 1)
-    ch2 = OPData.fromPath('../data/part2/%s.tab' % name, 2)
-    ch1.multiplyX(1000)
-    ch2.multiplyX(1000)
-
-    c = TCanvas('c-%s' % name, '', 1280, 720)
-
-    g1 = ch1.makeGraph('g1-%s' % name, 'Zeit t / ms', 'Spannung U / V')
-    prepareGraph(g1)
-    g1.SetMinimum(min(ch2.getY()) * 1.2)
-    g1.SetMaximum(max(ch2.getY()) * 1.2)
-    g1.GetXaxis().SetRangeUser(xmin, xmax)
-    g1.Draw('APX')
-    g2 = ch2.makeGraph('g2-%s' % name)
-    prepareGraph(g2)
-    g2.SetMarkerColor(2)
-    g2.Draw('PX')
-
-    fitStartParams = getHFSFitStartParams(name)
-    fitres = []
-    if fitStartParams:
-        peakNum = 0
-        offset = ch2.getY()[0]  # approx offset of underground
-        slope = (ch2.getY()[-1] - ch2.getY()[0]) / (xmax - xmin)  # approx slope of underground
-        for peakparams, xstartend in fitStartParams:
-            xstart, xend = xstartend
-            peakcount = len(peakparams)
-            fitfunc = 'pol1(0)+gaus(2)'
-            dpc = 2  # delta param count
-            for i in range(1, peakcount):
-                fitfunc += '+gaus(%d)' % (i * 3 + dpc)
-            fit = Fitter('fitHFS%s' % name, fitfunc)
-            fit.function.SetLineColor(peakNum + 3)
-            fit.setParam(0, 'a', offset)
-            fit.setParamLimits(0, 0, offset * 2)
-            fit.setParam(1, 'b', slope)
-            fit.setParamLimits(1, 0, 2 * slope)
-            for i, params in enumerate(peakparams):
-                fit.setParam(i * 3 + dpc, 'A%d' % i, params[0])
-                fit.setParam(i * 3 + dpc + 1, 'c%d' % i, params[1])
-                fit.setParam(i * 3 + dpc + 2, 's%d' % i, params[2])
-                if params[0] < 0:
-                    fit.setParamLimits(i * 3 + dpc, 3 * params[0], 0)
-                else:
-                    fit.setParamLimits(i * 3 + dpc, 0, 2 * params[0])
-                fit.setParamLimits(i * 3 + dpc + 1, params[1] - 2 * params[2], params[1] + 2 * params[2])
-                fit.setParamLimits(i * 3 + dpc + 2, 0, 20 * params[2])
-            fit.fit(g2, xstart, xend, '+')
-            fit.saveData('../fit/part2/%s-%d.txt' % (name, peakNum))
-            for i in range(len(peakparams)):
-                fitres.append((fit.params[i * 3 + dpc + 1]['value'], fit.params[i * 3 + dpc + 1]['error']))
-            peakNum += 1
-
-    g1.Draw('P')
-    g2.Draw('P')
-    c.Update()
-    if not DEBUG:
-        c.Print('../img/part2/%s_fit.pdf' % name, 'pdf')
-    return fitres
-
-
 def getHFSFitStartParams(name):
     params = dict()
     params['up-hfs_zoom'] = [[[(-0.1715, 0.0005724, 8.151e-6), (-0.0639, 0.0006404, 5.15e-6)], (0.0005191, 0.0007188)],
@@ -268,7 +206,7 @@ def makeHFSGraph(name, xmin, xmax):
     deltaY = 0.05
 
     c = TCanvas('c-%s' % name, '', 1280, 720)
-    g2 = ch2.makeGraph('g2-%s' % name, "Zeit t / s", "U_{ph} / V")
+    g2 = ch2.makeGraph('g2-%s' % name, "Zeit t / s", "Spannung U_{ph} / V")
     prepareGraph(g2, 2)
     g2.GetXaxis().SetRangeUser(xmin, xmax)
     g2.SetMinimum(ch2.getMinY() - deltaY)
@@ -279,6 +217,7 @@ def makeHFSGraph(name, xmin, xmax):
     fitres = []
     tablefitres = []
     tablepeakcount = 0
+    legendInfo = []
     if fitStartParams:
         print('got start params, starting to building fit functions')
         peakNum = 0
@@ -314,11 +253,14 @@ def makeHFSGraph(name, xmin, xmax):
                 fit.setParamLimits(i * 3 + dpc + 2, 0, 20 * params[2])
             fit.fit(g2, xstart, xend, 'M+')
             fit.saveData('../fit/part2/%s-%d.txt' % (name, peakNum))
+            legendpeaks = []
             for i in range(len(peakparams)):
                 fitres.append((fit.params[i * 3 + dpc + 1]['value'], 0.2 * fit.params[i * 3 + dpc + 2]['value']))
                 tablepeakcount += 1
+                legendpeaks.append(tablepeakcount)
                 tablefitres.append((tablepeakcount, fit.params[i * 3 + dpc + 1]['value'] * 1e3, fit.params[i * 3 + dpc + 1]['error'] * 1e3,
                                     fit.params[i * 3 + dpc + 2]['value'] * 1e6, fit.params[i * 3 + dpc + 2]['error'] * 1e6))
+            legendInfo.append((fit.function, tuple(legendpeaks)))
             peakNum += 1
         tablename = 'up' if isUp else 'down'
         with TxtFile('../src/tab_part2_hfspeaks_%s.tex' % tablename, 'w') as f:
@@ -329,8 +271,23 @@ def makeHFSGraph(name, xmin, xmax):
                                        "tab:hfs:peaks:%s" % tablename)
 
     g2.Draw('P')
+
+    l = TLegend(0.725, 0.15, 0.99, 0.45)
+    l.SetTextSize(0.03)
+    l.AddEntry(g2, 'Photodiodenspannung U_{ph}', 'l')
+    for fitfunc, legendpeaks in legendInfo:
+        n = len(legendpeaks)
+        if n == 1:
+            lstring = '%d' % legendpeaks
+        elif n == 2:
+            lstring = '%d und %d' % legendpeaks
+        elif n == 3:
+            lstring = '%d, %d und %d' % legendpeaks
+        l.AddEntry(fitfunc, "Fit von Peak %s" % lstring, 'l')
+    l.Draw()
+
     c.Update()
-    if not DEBUG:
+    if True or not DEBUG:
         c.Print('../img/part2/%s_fit.pdf' % name, 'pdf')
 
     return fitres
@@ -383,7 +340,7 @@ def evalIntervalConst(avgspectrum):
 
     def sub2vals(val1, val2):
         return val1[0] - val2[0], sqrt(val1[1] ** 2 + val2[1] ** 2)
-    
+
     s = avgspectrum[:2] + avgspectrum[-2:]
     sfreq = avg2vals(sub2vals(s[2], s[0]), sub2vals(s[3], s[1]))  # S delta freq
     pfreq = avg2vals(sub2vals(s[3], s[2]), sub2vals(s[1], s[0]))  # P delta freq
@@ -392,18 +349,18 @@ def evalIntervalConst(avgspectrum):
     names = [r"${}^2\text{S}_{1/2}$", r"${}^2\text{P}_{1/2}$"]
     litvals = ["14.13", "1.692"]
     As = []
-    for name, litval,  (freq, sfreq), F in zip(*[names, litvals, freqs, Fs]):
+    for name, litval, (freq, sfreq), F in zip(*[names, litvals, freqs, Fs]):
         A = freq * h_eVs / (F + 1) * 1e15  # in µeV and GHz in Hz
         sA = sfreq * h_eVs / (F + 1) * 1e15
         As.append([name, litval, A, sA])
     with TxtFile('../src/tab_part2_A.tex', 'w') as f:
-        f.write2DArrayToLatexTable(As, 
-                                   ["Feinstruktur", r"$A^\text{Lit.}$ / \textmu eV", 
-                                    r"$A^\text{exp}$ / \textmu eV", r"$s_{A^\text{exp}}$ / \textmu eV"], 
-                                   ['%s', '%s', '%.2f', '%.2f'], 
-                                   r"Errechnete HFS-Intervallkonstanten $A$ für die zwei untersten Feinstrukturniveus von \rb{87}.", 
+        f.write2DArrayToLatexTable(As,
+                                   ["Feinstruktur", r"$A^\text{Lit.}$ / \textmu eV",
+                                    r"$A^\text{exp}$ / \textmu eV", r"$s_{A^\text{exp}}$ / \textmu eV"],
+                                   ['%s', '%s', '%.2f', '%.2f'],
+                                   r"Errechnete HFS-Intervallkonstanten $A$ für die zwei untersten Feinstrukturniveus von \rb{87}.",
                                    "tab:hfs:intervalconsts")
-    
+
 
 def main():
     print('make graphs')
